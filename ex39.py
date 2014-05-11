13e18d3668866e866b92a54cbd866adfc74961d9
Barry Wainstock <barrywainstock@gmail.com>
2014-05-11 14:59:12 -0700

    Updated 39 and 40

--- ex39.py
+++ ex39.py
@@ -1,17 +1,21 @@
+# Create a mapping of state to abbreviation
+
 states = {
         'Oregon': 'OR',
         'Florida': 'FL',
         'California': 'CA',
         'New York': 'NY',
-        'Michigan': 'MI',
+        'Michigan': 'MI'
         }
 
+# create a basic set of states and some cities in them
 cities = {
-        'CA': 'SAn Francisco',
-        'MI': 'Detroit',
-        'FL': 'Jacksonville'
+        'CA': 'San Francisco',
+        'FL': 'Jacksonville',
+        'MI': 'Detroit'
         }
 
+# add more cities
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
@@ -20,27 +24,26 @@ print "NY State has: ", cities['NY']
 print "OR State has: ", cities['OR']
 
 print '-' * 10
-print "Michigan's abbreviation is: ", states['Michigan']
-print "Florida's abbreviation is: ", states['Michigan']
-
-print '-' * 10
-print "Michigan has: ", cities[states['Michigan']]
-print "Florida has: ", cities[states['Florida']]
-
-print '-' * 10
-for abbrev, city in cities.items():
-    print "%s has the city %s" % (abbrev, city)
+print "Michigan's abb is: ", states['Michigan']
+print "Florida's abb is: ", states['Florida']
 
 print '-' * 10
 for state, abbrev in states.items():
-    print "%s state is abbreviated %s and has city %s" % (state, abbrev,
-            cities[abbrev])
+        print "%s is abbreviated %s" % (state, abbrev)
+
+print '-' * 10
+for abbrev, city in cities.items():
+        print "%s has the city %s" % (abbrev, city)
+
+print '-' * 10
+for state, abbrev in states.items():
+        print "%s state is abbreviated %s and has city %s" % (state, abbrev, cities[abbrev])
 
 print '-' * 10
 state = states.get('Texas', None)
 
 if not state:
-    print "sorry, no Texas"
+        print "sorry, no Texas."
 
 city = cities.get('TX', 'Does Not Exist')
-print "The city for the state 'TX' is: %s" % city
+print "the city for the state TX is: %s" % city
--- ex40.py
+++ ex40.py
@@ -1,19 +1,21 @@
 class Song(object):
 
-    def __init__(self, lyrics):
-        self.lyrics = lyrics
+        def __init__(self, lyrics):
+                self.lyrics = lyrics
 
-    def sing_me_a_song(self):
-        for line in self.lyrics:
-            print line
+        def sing_me_a_song(self):
+                for line in self.lyrics:
+                        print line
 
-happy_bday = Song(["Happy birday to you",
-                    "I don't want to get sued",
-                    "This is th elast line"])
+lyrics_happy = ["Happy birthday to you", "I dont", "So I' stop"]
+happy_bday = Song(lyrics_happy)
 
-bulls_on_parade = Song(["They rally around the family",
-                        "With pockets full of shells"])
+lyrics_bulls = ["Rally around", "Shelllllls"]
+bulls_on_parade = Song(lyrics_bulls)
+
+print "Lyrics:"
+print bulls_on_parade.lyrics
 
 happy_bday.sing_me_a_song()
-
 bulls_on_parade.sing_me_a_song()
+
